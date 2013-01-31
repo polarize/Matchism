a13896ac7acacf014a2787f5f7b6a0ef835ee9bc
Issam Bendaas <polarware@gmail.com>
2013-01-31 22:36:35 +0100

    editing Deck Classe

--- Matchismo/Model/Deck.m
+++ Matchismo/Model/Deck.m
@@ -8,6 +8,42 @@
 
 #import "Deck.h"
 
+@interface Deck()
+@property (strong, nonatomic) NSMutableArray *cards; //of Card
+
+@end
+
 @implementation Deck
+
+-(NSMutableArray *)cards
+{
+    if (!_cards) {
+        _cards = [[NSMutableArray alloc]init];
+    }
+    return _cards;
+}
+
+-(void)addCard:(Card *)card atTop:(BOOL)atTop
+{
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    } else
+    {
+        [self.cards addObject:card];
+    }
+    
+}
+-(Card *)drawRandomCard
+{
+    Card *randomCard = nil;
+    
+    if (self.cards.count) {
+        unsigned index = arc4random() % self.cards.count;
+        randomCard = self.cards[index];
+        [self.cards removeObjectAtIndex:index];
+    }
+    return randomCard;
+    
+}
 
 @end
